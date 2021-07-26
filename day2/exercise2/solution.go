7187071831de2723a6c95d2cc8e03bbe8dc97895
Aniket Agarwalla <44133710+ani37@users.noreply.github.com>
2021-07-26 10:17:26 +0530

    made suggested changes

--- day2/exercise2/solution.go
+++ day2/exercise2/solution.go
@@ -7,42 +7,29 @@ import (
 	"time"
 )
 
-func allocate(noOfStudents int, ratings chan int) {
-	for i := 0; i < noOfStudents; i++ {
-		time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
-		rating := rand.Intn(10) + 1
-		ratings <- rating
-	}
-	close(ratings)
-}
+var output = 0
 
-func findAverage(wg *sync.WaitGroup,ratings chan int, average chan int){
+func allocate(wg *sync.WaitGroup) {
 
-	output :=0
-
-	for rating := range ratings {
-		output += rating
-	}
-
-	output /= cap(ratings)
-	average <- output
+	time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
+	rating := rand.Intn(10) + 1
+	output += rating
 	wg.Done()
 }
 
 func main(){
 
-	var ratings = make(chan int, 200)
-	var average = make(chan int, 1)
 
-	go allocate(200, ratings)
-
+	var noOfStudents = 200
 	var wg sync.WaitGroup
-	wg.Add(1)
 
-	go findAverage(&wg, ratings, average)
+	for i := 0; i < noOfStudents; i++ {
+		wg.Add(1)
+		go allocate(&wg)
+	}
 
 	wg.Wait()
+	result := output / noOfStudents
 
-	result := <- average
 	fmt.Println("the average rating given from the students is", result)
 }
